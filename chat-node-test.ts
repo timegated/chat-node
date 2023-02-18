5b0e8a7fe520e250431d97715d8d9e0fc47fdbd9
Daniel Beccaria <danielbeccaria@Daniels-MacBook-Pro.local>
2023-02-17 21:48:10 -0800

    Added answers and stream routes

---
+++ chat-node-test.ts
@@ -0,0 +1,98 @@
+import { WriteStream } from "fs";
+
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const config = new Configuration({
+  apiKey: process.env.GPT_SECRET,
+});
+const api = new OpenAIApi(config);
+
+// Insert before what a user types if they select this option.
+const promptPrepend = [
+    {
+      type: "WHAT",
+      prepend: "What is a",
+    },
+    {
+      type: "ROLE",
+      prepend: "Assume the role of a",
+    },
+    {
+      type: "EXP",
+      explain: "Explain",
+    },
+  ];
+
+export async function promptResponse(promptText: string, promptChoice: number, model: string) {
+    try {
+        const completion = await api.createCompletion({
+          model: model,
+          prompt: `${promptPrepend[promptChoice].prepend} ${promptText}`,
+          max_tokens: 4000
+        });
+        console.log(completion.data.choices);
+        return completion.data.choices[0].text;
+      } catch (error: any) {
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.data);
+        } else {
+          console.log(error.message);
+        }
+      }
+}
+
+export const extractLines = (data: Buffer) =>
+  data
+    .toString()
+    .split("\n")
+    .filter((line: string) => line.trim() !== "");
+
+export const parseStreamData = (data: Buffer) => {
+    const lines = extractLines(data);
+    for (const line of lines) {
+      const message = line.replace(/^data: /, "");
+      if (message === "[DONE]") {
+        return; // Stream finished
+      }
+      try {
+        const parsed = JSON.parse(message);
+        return parsed.choices[0].text;
+      } catch (error) {
+        console.error("Could not JSON parse stream message", message, error);
+      }
+    }
+}
+
+export async function promptResponseStream(prompt: string, prefixChoice: number, model: string) {
+  console.log(promptPrepend);
+  try {
+    const res = await api.createCompletion(
+      {
+        model: model,
+        prompt: `${promptPrepend[prefixChoice].prepend} ${prompt}`,
+        max_tokens: 1000,
+        temperature: 0,
+        stream: true,
+      },
+      { responseType: "stream" }
+    );
+    const result = res.data.on("data", (data: Buffer) => parseStreamData(data));
+    return result;
+  } catch (error: any) {
+    if (error.response?.status) {
+      error.response.data.on("data", (data: Buffer) => {
+        const message = data.toString();
+        try {
+          const parsed = JSON.parse(message);
+          console.error("An error occurred during OpenAI request: ", parsed);
+        } catch (error) {
+          console.error("An error occurred during OpenAI request: ", message);
+        }
+      });
+    } else {
+      console.error("An error occurred during OpenAI request", error);
+    }
+  }
+}
---
+++ index.ts
@@ -0,0 +1,76 @@
+import express from "express";
+import {
+  promptResponse,
+  promptResponseStream,
+} from "./chat-node-test";
+import { Readable, Transform, finished } from "stream";
+
+const app = express();
+const PORT = 3002;
+
+// util move out
+const streamOn = (result: any) => {
+  const readable = Readable.from(result);
+  const delay = new Transform({
+    transform(chunk, enc, cb) {
+        console.log(chunk);
+      setTimeout(cb, 100, null, chunk)
+    },
+  });
+  return readable.pipe(delay);
+};
+
+app.get("/answer", async (req: express.Request, res: express.Response) => {
+  try {
+    const { prompt, prefixChoice, modelChoice } = req.query;
+    if (!prompt) {
+      res.status(400).send("Prompt is required for request");
+    }
+    console.log(prompt);
+    const promptText = prompt ? String(prompt) : "";
+    const prefix = prefixChoice ? Number(prefixChoice) : 0;
+    const model = modelChoice ? String(modelChoice) : "text-davinci-002";
+
+    const result = await promptResponse(promptText, prefix, model);
+
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send("Bad Request");
+    throw error;
+  }
+});
+
+app.get(
+  "/stream",
+  async (req: express.Request, res: express.Response, next) => {
+    try {
+      const { prompt, prefixChoice, modelChoice } = req.query;
+      console.log(prompt, prefixChoice);
+      if (!prompt) {
+        res.status(400).send("Prompt is required for request");
+        return;
+      }
+      const promptText = prompt ? String(prompt) : "";
+      const prefix = prefixChoice ? Number(prefixChoice) : 0;
+      const model = modelChoice ? String(modelChoice) : "text-davinci-002";
+      const result = await promptResponseStream(promptText, prefix, model);
+      const stream = streamOn(result);
+      stream.pipe(res, { end: false });
+      finished(stream, (err) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        res.end();
+      });
+    } catch (error) {
+      // Logging at some point
+      res.status(400).send("Bad Request");
+      throw error;
+    }
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Openai experiment running on port: ${PORT}`);
+});
