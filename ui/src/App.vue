63fc43be144cb494ceaa310be9ed39b257418bcc
Daniel Beccaria <danielbeccaria@Daniels-MacBook-Pro.local>
2023-02-25 10:37:29 -0800

    Remove LGO

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,15 +4,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="prompts">Prompts</RouterLink>
       </nav>
-    </div>
   </header>
 
   <RouterView />
