4454b81cc306f638fa408bd5d6a8d0d451617401
Daniel Beccaria <danielbeccaria@Daniels-MacBook-Pro.local>
2023-02-24 18:55:07 -0800

    Fix error with prompt list

--- ui/src/components/PromptHandler.vue
+++ ui/src/components/PromptHandler.vue
@@ -1,34 +1,30 @@
-<template>
-    <h1>PromptHandler</h1>
-    <h1 class="user-title">{{ name }}</h1>
-    <section class="prompt-container">
-            <span :v-for="prompt in prompts" class="prompts" draggable="true">
-                {{ prompt.prompt }}
-            </span>
-    </section>
+<template :v-for="{ prompt } of prompts">
+    <span class="prompts" draggable="true">
+        {{ prompt }}
+    </span>
 </template>
 
 <script lang="ts">
 
 interface Data {
     name: string;
-    prompts: { prompt: string}[]
+    prompts: { prompt: string }[]
 };
 
 export default {
     data() {
         return {
-                name: 'John',
-                prompts: [
-                    {prompt: 'Explain SQL'},
-                    {prompt: 'Write a SQL query with at least three JOINS'},
-                    {prompt: 'Explain what a Join is'}
-                ]
-            } as Data;
+            name: 'John',
+            prompts: [
+                { prompt: 'Explain SQL' },
+                { prompt: 'Write a SQL query with at least three JOINS' },
+                { prompt: 'Explain what a Join is' }
+            ]
+        } as Data;
     },
     computed: {
         getPrompts() {
-            return !this.prompts.length ? [{prompt: 'No Prompts Here'}] : this.prompts;
+            return !this.prompts.length ? [{ prompt: 'No Prompts Here' }] : this.prompts;
         },
         mapOverPrompts() {
             return this.getPrompts.map((prompt) => prompt.prompt).join(' ');
@@ -41,6 +37,7 @@ export default {
 .user-title {
     width: 20%;
 }
+
 .prompt-container {
     display: flex;
     flex-direction: column;
