3f4a0438919fd439f12403f68c0461afefdb8f90
timegated <beccdaniel@gmail.com>
2023-03-26 22:22:54 -0700

    Cleanup vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueSvg from '@vuetter/vite-plugin-vue-svg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueSvg()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
